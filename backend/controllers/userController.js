1e87ccbce69adecf90221af38e27a45b3ee64967
John Rusty Lola <johnrustylola@gmail.com>
2025-01-08 17:51:27 +0800

    fix update avatar user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,7 +2,8 @@ import { StatusCodes } from "http-status-codes";
 import Job from "../models/jobModel.js";
 import User from "../models/userModel.js";
 import cloudinary from 'cloudinary';
-import {promises as fs} from 'fs';
+import { formatImage } from "../middleware/multerMiddleware.js";
+// import {promises as fs} from 'fs';
 
 
 export const getCurrentUser = async (req, res) => {
@@ -17,8 +18,11 @@ export const updateUser = async (req, res) => {
     delete newUser.password;
 
     if(req.file){
-        const response = await cloudinary.v2.uploader.upload(req.file.path);
-        await fs.unlink(req.file.path);
+
+        const file = formatImage(req.file);
+        const response = await cloudinary.v2.uploader.upload(file);
+        // await fs.unlink(req.file.path);
+
         newUser.avatar = response.secure_url;
         newUser.avatarPublicId = response.public_id;
     }
